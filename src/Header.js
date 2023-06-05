7d7f1ab4559d27adcc61555c1b2e812b13e99711
yassin <yassinhussein241@gmail.com>
2023-06-05 14:41:49 -0400

    Edit Header to Make it Responsive

--- src/Header.js
+++ src/Header.js
@@ -2,31 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <>
-      <div className="header">
-        <div className="header--name">
-          <Link to="/" className="header--name--button">
-            Yassin
-          </Link>
-        </div>
-        <div>
-          <Link to="/about" className="button">
-            About
-          </Link>
-          <Link to="/skills" className="button">
-            Skills & Tools
-          </Link>
-          <Link to="/projects" className="button">
-            Projects
-          </Link>
-          <Link to="/resume" className="button">
-            Resume
-          </Link>
-          <Link to="/contact" className="button">
-            Contact
-          </Link>
-        </div>
+    <header className="header">
+      <div className="header--name">
+        <Link to="/" className="header--name--button">
+          Yassin
+        </Link>
       </div>
-    </>
+      <nav>
+        <Link to="/about" className="button">
+          About
+        </Link>
+        <Link to="/skills" className="button">
+          Skills & Tools
+        </Link>
+        <Link to="/projects" className="button">
+          Projects
+        </Link>
+        <Link to="/resume" className="button">
+          Resume
+        </Link>
+        <Link to="/contact" className="button">
+          Contact
+        </Link>
+      </nav>
+    </header>
   );
 }
