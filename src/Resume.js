bb6703936107ae384c16e2d1d84035ced94e9892
yassin <yassinhussein241@gmail.com>
2023-06-05 10:54:18 -0400

    Add Resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import YassinResume from "../public/YassinResume.pdf";
 export default function Resume() {
   return (
     <div className="container">
-
       <Header />
+      <h1>Resume</h1>
+      <a href={YassinResume} download>
+        Download Resume
+      </a>
       <Footer />
     </div>
   );
