104adcf7ed0a649f5e32cf8d4d2be6b804debffb
yassin <yassinhussein241@gmail.com>
2023-06-05 14:08:18 -0400

    Edit Greeting Message to Make it Responsive

--- src/GreetingMessage.js
+++ src/GreetingMessage.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import greetings from './Greetings';
+import React, { useState, useEffect, useCallback } from "react";
+import greetings from "./Greetings";
 
 const GreetingMessage = () => {
   const [currentLanguage, setCurrentLanguage] = useState(0);
   const languageKeys = Object.keys(greetings);
 
   const nextLanguage = useCallback(() => {
-    setCurrentLanguage((prevLanguage) => (prevLanguage + 1) % languageKeys.length);
+    setCurrentLanguage(
+      (prevLanguage) => (prevLanguage + 1) % languageKeys.length
+    );
   }, [languageKeys.length]);
 
   useEffect(() => {
@@ -14,14 +16,22 @@ const GreetingMessage = () => {
   }, [nextLanguage]);
 
   return (
-    <h1 className='greetings'>
-      {languageKeys.map((languageKey, index) => (
-        <React.Fragment key={index}>
-          {index !== 0 && <span className='greeting-space'></span>}
-          <span className={`greeting ${currentLanguage === index ? 'active' : ''}`}>{greetings[languageKey]}</span>
-        </React.Fragment>
-      ))}
-    </h1>
+    <div className="greeting-container">
+      <h1 className="greetings">
+        {languageKeys.map((languageKey, index) => (
+          <React.Fragment key={index}>
+            {index !== 0 && <span className="greeting-space"></span>}
+            <span
+              className={`greeting ${
+                currentLanguage === index ? "active" : ""
+              }`}
+            >
+              {greetings[languageKey]}
+            </span>
+          </React.Fragment>
+        ))}
+      </h1>
+    </div>
   );
 };
 
